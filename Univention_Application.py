332c2537fd941295c8c09bed93c954babae3e45a
Tobias Walter <towalter@uni-bremen.de>
2020-07-01 13:47:10 +0200

    Updated Commentary

--- Univention_Application.py
+++ Univention_Application.py
@@ -7,12 +7,10 @@ print("I'd like to play a game, bet you can't guess the number im thinking of? A
 
 while rndInt != guess:              # while generated number is not equal to guess    
     guess = int(input("Guess?"))    # get user input
-    if guess < rndInt:              
+    if guess < rndInt:              #compare guess and randomInt
         print ("nah, too small, try again.")   
     elif guess > rndInt:
         print ("Really? Too large my friend.")      
-    if guess == rndInt:
-        break
         
 print("Yep! You're right. Good job")
         
